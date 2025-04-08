3f29ea2570538946c2c9ad456ece626f3e74cbb4
GuilhSchneider <160530132+GuilhSchneider@users.noreply.github.com>
2025-04-07 21:48:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,26 @@
 const express = require('express');
 const path = require('path');
-const req = require('express/lib/request');
-const res = require('express/lib/response');
 
-
-const app = require('express')();
-
+const app = express();
 const http = require('http').createServer(app);
-
 const io = require('socket.io')(http);
 
-app.use(express.static(__dirname))
+// Serve os arquivos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Rota principal
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+);
 
-app.get('/',(req,res) =>
-
-res.sendFile(path.join(__dirname, 'index.html')));
-
-
+// WebSocket
 io.on('connection', (socket) => {
   console.log('Usuário conectado');
 
-
   socket.on('chat message', (data) => io.emit('chat message', data));
-
 
   socket.on('disconnect', () => console.log('Usuário desconectado'));
 });
-
 
 http.listen(3000, () => {
   console.log(`Servidor rodando na porta 3000 - Link http://localhost:3000`);
